30d5258f28a34ce0f35cb136806b8c9602989a37
Tomasz Tracz <t@tracz.me>
2022-09-06 15:30:36 +0100

    Add test code

--- cypress/e2e/Tests/myfirsttest.cy.js
+++ cypress/e2e/Tests/myfirsttest.cy.js
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("empty spec", () => {
+  it.only("passes", () => {
+    // cy.pause();
+    cy.visit("https://www.edgewordstraining.co.uk/demo-site");
+    cy.get(".woocommerce-store-notice__dismiss-link").click();
+    cy.get("#woocommerce-product-search-field-0").type("cap{enter}");
+  });
+
+  it("is another test", () => {
+    cy.visit("https://google.com");
+    cy.get("#W0wltc > .QS5gu").click();
+  });
+});
