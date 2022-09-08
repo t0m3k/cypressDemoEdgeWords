f0aea8e84325492da078ea6acd2d53255438678f
Tomasz Tracz <t@tracz.me>
2022-09-08 15:16:32 +0100

    Add junit reporting

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "ndmvsm",
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "cypress/results/junit-result-[hash].xmpl",
+    toConsole: true,
+  },
   e2e: {
     baseUrl: "https://www.edgewordstraining.co.uk/demo-site",
     setupNodeEvents(on, config) {
