c83048bde9c6c28066e2d08c3567e6ad5b2a8cda
Tomasz Tracz <t@tracz.me>
2022-09-09 15:48:17 +0100

    Add fixtures array example

--- cypress/e2e/Tests/usingFixtures.cy.js
+++ cypress/e2e/Tests/usingFixtures.cy.js
@@ -1,4 +1,4 @@
-describe("Using fixture and env vars", () => {
+/* describe("Using fixture and env vars", () => {
   beforeEach(function () {
     cy.fixture("loginData2").then((data) => {
       [this.username, this.password] = data.split(",");
@@ -24,6 +24,30 @@ describe("Using fixture and env vars", () => {
 
     cy.on("window:confirm", (str) => {
       return false;
+    });
+  });
+}); */
+
+describe("Use an array fixture", function () {
+  const data = require("../../fixtures/loginData.json");
+
+  data.forEach((credentials) => {
+    it("Login multiple times using data from array", function () {
+      cy.viewport("iphone-xr");
+      cy.visit("https://www.edgewordstraining.co.uk/webdriver2");
+      cy.contains("Login To Restricted Area").click();
+
+      cy.get("#username").type(credentials.username);
+
+      cy.get("#password").type(credentials.password);
+
+      cy.contains("Submit").click();
+
+      cy.contains("Log Out").click();
+
+      cy.on("window:confirm", (str) => {
+        return false;
+      });
     });
   });
 });
