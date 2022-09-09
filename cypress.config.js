03a2924162eec5c38cda9c7fe3d7b2dd727e0c78
Tomasz Tracz <t@tracz.me>
2022-09-09 14:30:33 +0100

    Add mocha reporting code

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,51 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "ndmvsm",
-  reporter: "junit",
+  reporter: "cypress-multi-reporters",
   reporterOptions: {
-    mochaFile: "cypress/results/junit-result-[hash].xml",
-    toConsole: true,
+    reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter",
+    cypressMochaawesomeReporterReporterOptions: {
+      reportDir: "cypress/reports",
+      charts: true,
+      reportPageTitle: "Steve's Report",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/reports/junit/results-[hash].xml",
+    },
   },
+
+  // reporter: "junit",
+  // reporterOptions: {
+  //   mochaFile: "cypress/results/junit-result-[hash].xml",
+  //   toConsole: true,
+  // },
   e2e: {
     baseUrl: "https://www.edgewordstraining.co.uk/demo-site",
     setupNodeEvents(on, config) {
+      const {
+        beforeRunHook,
+        afterRunHook,
+      } = require("cypress-mochawesome-reporter/lib");
+      const exec = require("child_process").execSync;
+
       // implement node event listeners here
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await exec("rm -rf cypress/screenshots");
+
+        await exec("rm -rf cypress/reports");
+
+        await beforeRunHook(details);
+      });
+      on("after:run", async () => {
+        console.log("override after:run");
+        await exec(
+          "npx jrm ./cypress/reports/junitreport.xml ./cypress/reports/junit/*.xml"
+        );
+        await afterRunHook();
+      });
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -13,13 +13,14 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-require("cypress-grep")();
-
 // Import commands.js using ES2015 syntax:
+
 import "./commands";
+import "cypress-mochawesome-reporter/register";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+require("cypress-grep")();
 
 beforeEach(function () {
   cy.log("Runs before each test from e2e support");
